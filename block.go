7473e9b25ef565b63a29b7bb6f68d5bb00bd913f
FaizanPervaz <faizan.pervaz@gmail.com>
2024-02-29 21:58:50 +0500

    Code Corrected

--- block.go
+++ block.go
@@ -29,30 +29,25 @@ func DisplayAllBlocks(chain Blockchain) {
 }
 
 func NewBlock(data string, prevHash string, chain Blockchain) Block {
-	// Generate a new block with the given data and previous block hash
 	newIndex := len(chain.Blocks) + 1
 	newBlock := Block{
 		Index:     newIndex,
 		Timestamp: time.Now(),
 		Data:      data,
 		PrevHash:  prevHash,
-		Hash:      "", // Placeholder for hash calculation
+		Hash:      "", 
 	}
 	newBlock.Hash = calculateHash(newBlock)
 	return newBlock
 }
 
 func ModifyBlock(block Block, newData string) Block {
-	// Modify the data of a block
 	block.Data = newData
-	// Recalculate the hash
 	block.Hash = calculateHash(block)
 	return block
 }
 
 func calculateHash(block Block) string {
-	// This is a placeholder function for calculating the hash of a block
-	// In a real-world scenario, you'd use a cryptographic hash function like SHA256
 	return "fakehash"
 }
 
